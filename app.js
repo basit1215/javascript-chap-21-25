8165c04c153aa6ea8e2a41c8dbbd5a6593bbc86b
basit1215 <haizabdulbasit225@gmail.com>
2024-04-01 01:51:31 +0500

    updated

---
+++ app.js
@@ -0,0 +1,5 @@
+// var firstName= prompt("Enter your First name...");
+// var lastName= prompt("Enter your Last name...");
+// var fullName= firstName+lastName;
+// alert("You Full name is " + fullName);
+
