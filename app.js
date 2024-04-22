fffeb05f4b374ab8e85ab5c584fb3f562a1cea37
basit1215 <haizabdulbasit225@gmail.com>
2024-04-23 02:55:28 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,61 @@
-// var firstName= prompt("Enter your First name...");
-// var lastName= prompt("Enter your Last name...");
-// var fullName= firstName+lastName;
-// alert("You Full name is " + fullName);
+var firstName= prompt("Enter your First name...");
+var lastName= prompt("Enter your Last name...");
+var fullName= firstName+" " + lastName;
+alert("You Full name is " + fullName);
+
+var userInp= prompt("Enter your favorite mobile phone model");
+document.write("My favorite mobile phone is: " + userInp + "<br>");
+document.write("Lengtj of string: " + userInp.length+ "<br>"+ "<br>");
+
+var country= "Pakistani";
+document.write("String: " + country+ "<br>");
+document.write("Index of 'n': " + country.indexOf("n") + "<br>"+ "<br>");
+
+var world= "Hello World";
+document.write("String: " + world+ "<br>");
+document.write("Last index of 'l': " + world.lastIndexOf("l") + "<br>"+ "<br>");
+
+var country= "Pakistani";
+document.write("String: " + country+ "<br>");
+document.write("Character at index 3: " + country.charAt(3) + "<br>"+ "<br>");
+
+var firstName= prompt("Enter your First name...");
+var lastName= prompt("Enter your Last name...");
+var fullName= firstName.concat(" " +lastName)
+alert("You Full name is " + fullName);
+
+
+var cityName= "Hyderabad";
+document.write("City: " + cityName+ "<br>");
+document.write("After replacement: "  + cityName.replace("Hyder", "Islam")+ "<br>"+ "<br>");
+
+var msg="Ali and Sami are best friends. They play cricket and football together.";
+document.write(msg+ "<br>");
+document.write("<b>" + "After replacing" + "</b>"+ "<br>");
+document.write(msg.replaceAll("and","&")+ "<br>"+ "<br>");
+
+
+
+
+
+
+var userInput= prompt("Enter small and capital alphabet, \ntrying all character to convert Uppercase");
+document.write(userInp+ "<br>");
+document.write(userInput.toUpperCase() + "<br>"+ "<br>");
+
+// var userInput2= prompt("Enter any word");
+// document.write(userInp2+ "<br>");
+// document.write(userInput2.() + "<br>"+ "<br>");
+
+
+
+
+
+
+
+
+
+
+
+
 
